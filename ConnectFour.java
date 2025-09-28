6d1f9b60246e57ee7a4c8efac0e4cfdfde14e40d
Alex <alexsab1515@gmail.com>
2025-09-28 01:11:12 -0400

    Added a few more helper methods before adding win check.

--- ConnectFour.java
+++ ConnectFour.java
@@ -11,9 +11,16 @@ public class ConnectFour {
 	private static int movesPlayed = 0; //	keep track of moves played for tie game
 	
 	public static void initBoard() {							// Fills up the board with dashes.
-        for (int r = 0; r < 6; r++)
-            for (int c = 0; c < 7; c++)
-                board[r][c] = '-';
+        for (int r = 0; r < 6; r++) {
+        	for (int c = 0; c < 7; c++) {
+            	board[r][c] = '-';
+            }
+        }
+        
+		/*
+		 * for (int r = 0; r < 6; r++) { for (int c = 6; c < 7; c++) { board[r][c] =
+		 * '-'; } }
+		 */
     }
 	
 	public static void displayBoard() {							// Displays board as 6 row and 7 columns, with numbers indicating the column on top for player move.
@@ -25,6 +32,18 @@ public class ConnectFour {
 		    }
 			System.out.println();
 		}    
+	}
+	
+	public static void setup() {	
+		initBoard();	//	set board to - on start or for new game
+		displayBoard();
+		System.out.println("WELCOME TO ALEXANDRE'S CONNECT 4!");
+		System.out.println("White (w) goes first, Black (b) goes second.");
+	}
+	
+	public static int getIntInput() {
+		
+		return 0;
 	}
 	
 	public static int getFreeColumn (int column, char color) {		// Modify the board with player move.
@@ -48,7 +67,7 @@ public class ConnectFour {
 				colorLetter = 'w';
 			}
 	 }
-	 
+
 	public static void modifyBoard (int move) {
 		int freeRow = getFreeColumn(move, colorLetter);
 		if (freeRow > -1) {
@@ -58,43 +77,48 @@ public class ConnectFour {
 			//System.out.println(movesPlayed);
 		} else {
 			System.out.println("Column is full! Choose another number.");
-			//continue;
 		}
 	}
+	
+	 public static boolean tieCheck() {
+		 if (movesPlayed == 42) {
+				System.out.println("Tie game.");
+				System.out.println();
+				return true;
+		 }
+		 return false;
+	 }
+	 
+	 public static boolean winCheck() {
+		 return false;
+	 }
 
 	public static void main(String[] args) {
-    
     	try (Scanner input = new Scanner(System.in)) {	//	create scanner
         	//SETUP LOOP//
         	while (true) {    // repeat while true
-        		
-        		initBoard();	//	set board to - on start or for new game
-        		displayBoard();
-        		
-        		//GAME LOOP//
-      
-        		System.out.println("WELCOME TO ALEXANDRE'S CONNECT 4!");
+        		setup();
         		while (true) {	 //	repeat while true)
     				System.out.println("Enter number between 1-7:");  										
             		try {
             			//get player input as int
-            			String line = input.nextLine().trim();  								// Attempts to get int from input. Prevents input with spaces to be accepted and allow one player to make multiple moves at once.
+            			String line = input.nextLine().trim();  								// Attempts to get int from input. Along with try catch prevents input with spaces to be accepted and let a player make multiple moves at once.
             			int move = Integer.parseInt(line); 										 
             			System.out.println();
-            			System.out.println("You entered: " + move);	 //	print input
-            	
             			
+            	
             			if (move < 1 || move > 7) {												// Only take full numbers between 1 - 7.
             			    System.out.println("Invalid number. Please choose a number between 1-7.");
             			    continue;
             			}
-            			
             			modifyBoard (move);
             			displayBoard();
-                		
-            			if (movesPlayed == 42) {
-            				System.out.println("Tie game.");
-            				System.out.println();
+            			System.out.printf("Player %s entered: %d%n" , colorLetter, move);	 //	print input
+            			
+            			if (winCheck()) {
+            				break;
+            			}
+            			if (tieCheck()) {
             				break;
             			}
             		} catch (Exception e) {
@@ -103,7 +127,21 @@ public class ConnectFour {
             		        input.next(); 									// Only consume when it's an input mismatch, like a single letter or character.
             		    }
             		}
-            	}   		
+            	}
+        		System.out.println("Play again? (y/n)");
+        		String playAgain = input.next();
+        		while (!(playAgain.equals("y") || playAgain.equals("n"))) {
+        			System.out.println("Only enter y or n.");
+        			System.out.println("Play again? (y/n");
+        			input.next();
+        		}
+        		if (playAgain.equals("y")) {
+        			movesPlayed = 0;
+        			input.nextLine();
+        			continue;
+        		} else {
+        			break;
+        		}
         	}
         }
 	}
